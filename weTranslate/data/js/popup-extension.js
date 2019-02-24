e8acc1965cf6c7483f2e940e5187218ee7b27792
Mohamed Arradi-Alaoui <arradi.mohamed@gmail.com>
2019-02-24 23:37:26 +0400

    Add Local Storage

--- weTranslate/data/js/popup-extension.js
+++ weTranslate/data/js/popup-extension.js
@@ -3,17 +3,19 @@
 function notifyExtension(event) {
     var targetElement = event.target || event.srcElement;
     if (targetElement.id == "translate_button") {
-        var value = document.getElementById('data-language').value;
+        var lang = document.getElementById('data-language').value;
         var engine = document.getElementById('data-engine').value;
         var newTabOption = document.getElementById('new_tab_checkbox').checked;
 
-        if (value != "0") {
+        if (lang != "0") {
             browser.runtime.sendMessage({
-                "data": value,
+                "data": lang,
                 "engine": engine,
                 "newTab": newTabOption
             });
         }
     }
 }
+
+///
 window.addEventListener("click", notifyExtension);
